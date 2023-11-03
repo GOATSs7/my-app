1e7338f92e4de78e74211ae9c8f228638a901658
sangram <sangramdadasochopdar@gmail.com>
2023-11-03 14:04:14 +0530

    added lowercase btn

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,10 +6,16 @@ import React ,{useState} from 'react'
 
 export default function TextForm(props) {
 
-  const uppercase =()=>{
+  const Uppercase =()=>{
     
    
     let newText=text.toUpperCase();
+    setText(newText)
+  }
+  const Lowercase =()=>{
+    
+   
+    let newText=text.toLowerCase();
     setText(newText)
   }
   
@@ -18,12 +24,12 @@ export default function TextForm(props) {
     setText(event.target.value)
 
   }
-  const [text, setText]=useState('Enter text here')
+  const [text, setText]=useState('')
 
     return ( 
 
-
-      <div>
+<>
+      <div className='container'>
         
        <h1>{props.heading}</h1>
         <div className='mb-3'>
@@ -31,8 +37,18 @@ export default function TextForm(props) {
 
       </div>
        
-    <button className='btn btn-primary' onClick={uppercase} >Convert to Uppercase</button>
+    <button className='btn btn-primary mx-1' onClick={Uppercase} >Convert to Uppercase</button>
+    <button className='btn btn-primary mx-1' onClick={Lowercase} >Convert to Lowercase</button>
 
        </div>
+       <div className="container my-3">
+        <h2>Your Text Summary</h2>
+         <p>{text.split(" ").length} words and {text.length} characters </p>
+         <p>{(Math.round(0.008*text.split(" ").length)).toFixed(2)} Minutes Read</p>
+       </div>
+         <h2>Preview</h2>
+         <p>{text} </p>
+</>
+      
     )
 }
