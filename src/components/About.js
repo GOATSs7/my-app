5a6642dee36354e7e235d9689491683dc53ecd58
sangram chopdar <sangramdadasochopdar@gmail.com>
2023-11-07 13:21:47 +0530

    Update About.js
    about updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,131 +1,107 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function About() {
-  const [myStyle, setStyle] = useState({
-    color: "black",
-    backgroundColor: "white",
-  });
+  // const [myStyle, setStyle] = useState({
+  //   color: "black",
+  //   backgroundColor: "white",
+  // });
 
-  const [myBtn, myBtnText] = useState("Enable Dark Mode");
+  // const [myBtn, myBtnText] = useState("Enable Dark Mode");
 
-  const toggleStyle = () => {
-    if (myStyle.color === "white") {
-      setStyle({
-        color: "black",
-        backgroundColor: "white",
-      });
-      myBtnText("Enable Dark Mode");
-    } else {
-      setStyle({
-        color: "white",
-        backgroundColor: "black",
-      });
-      myBtnText("Enable Light Mode");
-    }
-  };
+  // const toggleStyle = () => {
+  //   if (myStyle.color === "white") {
+  //     setStyle({
+  //       color: "black",
+  //       backgroundColor: "white",
+  //     });
+  //     myBtnText("Enable Dark Mode");
+  //   } else {
+  //     setStyle({
+  //       color: "white",
+  //       backgroundColor: "black",
+  //     });
+  //     myBtnText("Enable Light Mode");
+  //   }
+  // };
 
   return (
-    <div>
-      <div className="accordion" id="accordionExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-              style={myStyle}
-            >
-              Accordion Item #1
-            </button>
-          </h2>
-          <div
-            id="collapseOne"
-            className="accordion-collapse collapse show"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={myStyle}>
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-              style={myStyle}
-            >
-              Accordion Item #2
-            </button>
-          </h2>
-          <div
-            id="collapseTwo"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={myStyle}>
-              <strong>This is the second item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-              style={myStyle}
-            >
-              Accordion Item #3
-            </button>
-          </h2>
-          <div
-            id="collapseThree"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={myStyle}>
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
+    <>
+      <div className="container">
+        <h1> About TextUtil</h1>
+        <br />
+        <br />
+        <b>
+          Welcome to TextUtil, your all-in-one text processing and analysis
+          tool! TextUtil is designed to assist you in managing, analyzing, and
+          enhancing your text content effortlessly. Whether you're a writer,
+          student, researcher, or content creator, TextUtil is here to simplify
+          your text-related tasks.
+        </b>
+        <br />
+        <br />
+        <b>Key Features:</b>
+        <br />
+        <br />
+        Text Processing: TextUtil allows you to perform essential text
+        operations like converting text to uppercase, lowercase, and removing
+        extra spaces. Make your text look exactly the way you want it.
+        <br />
+        <br />
+        <b> Word Count:</b> Easily count the number of words in your text.
+        Useful for essays, reports, or any text-based project.
+        <br />
+        <br />
+        <b> Character Count:</b> Determine the total character count in your
+        text, helping you stay within character limits, especially useful for
+        social media posts.
+        <br />
+        <br />
+        <b> Text Analysis:</b> Discover the word count, character count, and
+        estimated reading time for your text. Tailor your content for the
+        perfect reading experience.
+        <br />
+        <br />
+        <b> Copy and Paste:</b> Conveniently copy and paste your text for
+        further use. No more manual copying or pasting &ndash; we've got you
+        covered.
+        <br />
+        <br />
+        <b> Remove Extra Spaces:</b> Tired of extra spaces messing up your
+        formatting? Use TextUtil to eliminate them with a single click.
+        <br />
+        <br />
+        <b> Focus Mode:</b> Need distraction-free writing? Engage our Focus Mode
+        and keep your concentration at its peak.
+        <br />
+        <br />
+        <b> Customization:</b> TextUtil offers a dark and light mode, so you can
+        work comfortably day or night.
+        <br />
+        <br />
+        <h3> Why TextUtil?</h3>
+        <br />
+        <br />
+        We understand the importance of efficient text management. TextUtil is
+        your trusted companion for text-related tasks. With its user-friendly
+        interface and powerful features, you can streamline your text processing
+        and make your content shine.
+        <br />
+        <br />
+        <b> Get Started</b>
+        <br />
+        <br />
+        Ready to enhance your text? Simply enter your text in the provided
+        field, choose from the available options, and let TextUtil work its
+        magic. It's that easy!
+        <br />
+        <br />
+        Discover the world of hassle-free text processing and analysis with
+        TextUtil. Try it out now and experience the difference.
+        <br />
+        <br />
+        Thank you for choosing <b> TextUtil</b>. We're here to simplify your
+        text journey.
       </div>
-      <div className="container my-3">
-        <button type="button" className="btn btn-primary" onClick={toggleStyle}>
-          {myBtn}{" "}
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
